e2e0a6ea1ec553d5094d729c1ea5bdf315ff32d6
Ruslan <pupsor@gmail.com>
2012-09-05 16:02:20 -0700

    new version with errorchecking_malloc

--- hacking/heap.c
+++ hacking/heap.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void *errorchecking_malloc(unsigned int);
 
 int main(int argc, char *argv[])
 {
@@ -14,27 +15,16 @@ int main(int argc, char *argv[])
       mem_size = atoi(argv[1]);
 
    printf("\t[+] allocation %d bytes of memory on the heap for char_ptr\n", mem_size);
-   char_ptr = (char *) malloc(mem_size);
+   char_ptr = (char *) errorchecking_malloc(mem_size);
    // test
    //
-   char_ptr2 = (char *) malloc(mem_size);
-   if(char_ptr == NULL)
-   {
-      fprintf(stderr, "Error: coulnd not allocate heap memory.\n");
-      exit(-1);
-   }
+   char_ptr2 = (char *) errorchecking_malloc(mem_size);
 
    strcpy(char_ptr, "This is memory is located on the heap.");
    printf("char_ptr (%p) --> '%s'\n", char_ptr, char_ptr);
    printf("char_ptr (%p) --> '%s'\n", char_ptr2, char_ptr2);
    printf("\t[+] allocation 12 bytes of memory on the heap for int_ptr\n");
-   int_ptr = (int *) malloc(12);
-
-   if(int_ptr == NULL)
-   {
-      fprintf(stderr, "Error: could not allocate heap memory");
-      exit(-1);
-   }
+   int_ptr = (int *) errorchecking_malloc(12);
 
    *int_ptr = 31337;
    printf("int_ptr (%p) --> %d\n", int_ptr, *int_ptr);
@@ -43,13 +33,7 @@ int main(int argc, char *argv[])
    free(char_ptr);
 
    printf("\t[+] allocation another 15 bytes for char_ptr\n");
-   char_ptr = (char *) malloc(15);
-
-   if(int_ptr == NULL)
-   {
-     fprintf(stderr, "Error: could not allocate heap memory");
-     exit(-1);
-   }
+   char_ptr = (char *) errorchecking_malloc(15);
 
    strcpy(char_ptr, "new memory");
    printf("char_ptr (%p) --> '%s'\n", char_ptr, char_ptr);
@@ -59,4 +43,16 @@ int main(int argc, char *argv[])
    free(char_ptr);
    free(char_ptr2);
    return 0;
+}
+
+void *errorchecking_malloc(unsigned int p)
+{  
+   void *ptr;
+   ptr = malloc(p);
+   if(ptr == NULL)
+   {
+      fprintf(stderr, "Eroor: could not allocate heap memory.\n");
+      exit(-1);
+   }   
+   return ptr;
 }
